2032e5b08d042baa9274e4bc67e021d99e4bb365
Dmitry Sky <dmitry.sky@icloud.com>
2016-11-09 13:16:01 -0700

    create a navigation panel and nest our routes under App

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from  'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!!!</div>
+    return (
+      <div>
+        <h3>React Router Tutorial</h3>
+        <ul role="nav">
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/repos">Repos</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
   }
 })
