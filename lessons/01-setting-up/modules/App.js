f941210ae6c3a5685e0e5e851cb16de4c5d91ec8
Dmitry Sky <dmitry.sky@icloud.com>
2016-11-10 11:41:51 -0700

    create IndexRoute to only render on home page

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,6 +1,7 @@
 import   React   from 'react'
 import { Link }  from  'react-router'
 import   NavLink from './NavLink'
+import   Home    from './Home'
 
 export default React.createClass({
   render() {
